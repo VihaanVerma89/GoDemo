d187c9488d523e4c3201db6208d51b40d367159a
vihaan <vihaanverama24@gmail.com>
2018-10-20 21:36:17 +0530

    first commit

---
+++ GoApp.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
+func main() {
+	router := mux.NewRouter()
+
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+
+	router.HandleFunc("/people", GetPeople).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(people)
+}
+
+func DeletePerson(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	for index, item := range people{
+		if item.ID == vars["id"]{
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(writer).Encode(people)
+}
+
+func CreatePerson(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	var person Person
+	_ =json.NewDecoder(request.Body).Decode(&person)
+	person.ID = vars["id"]
+	people = append(people, person)
+	json.NewEncoder(writer).Encode(people)
+}
+
+func GetPerson(writer http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	for _, item := range people{
+		if item.ID == vars["id"] {
+			json.NewEncoder(writer).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(writer).Encode(&Person{})
+}
